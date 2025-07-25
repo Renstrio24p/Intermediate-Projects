54e457a6a85bc567dfaae36d3040bc32b81e258c
Waren Gador <waren.gador15@gmail.com>
2025-07-25 14:41:50 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import checker from 'vite-plugin-checker';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    build: {
-        ssr: 'src/entry-server.ts',
-        outDir: 'dist/server'
-    },
+
     plugins: [tailwindcss(), checker({ typescript: true })],
 })
