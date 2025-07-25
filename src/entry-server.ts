d8ec315b8d15f0fcc6af275d7f526f7f44aefff9
Waren Gador <waren.gador15@gmail.com>
2025-07-25 16:30:51 +0800

    update

--- src/entry-server.ts
+++ src/entry-server.ts
@@ -1,8 +1,15 @@
 import html from "./utils/define/html"
 
-export function render() {
-  const htmls = html`
+export function render(_url: string): { html: string; head?: string } {
+  const htmlStr = html`
     <div id="app" class='w-screen'></div>
   `
-  return { htmls }
+  return {
+    html: htmlStr,
+    head: `
+      <meta name="description" content="Chatbot Assistant powered by OpenAI" />
+      <meta name="author" content="Your Name" />
+      <title>Chatbot Assistant Open AI</title>
+    `
+  }
 }
